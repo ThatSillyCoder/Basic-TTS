69932e757f83894c16e1d4dc1c1fc252060e71ac
ASillyLittleCoder <souid.youssef07@gmail.com>
2025-08-26 17:14:34 +0100

    Add files via upload

--- TTS.py
+++ TTS.py
@@ -1,10 +1,17 @@
 import pyttsx3
 import os
+from sys import exit
 sx3 = pyttsx3.init()
-_user = f"C:\\Users\\{os.getlogin()}\\"
+_type = input(" Drive or Computer ( g / c ) >> ")
+_request = input(" Insert Audio >> ")
+_wav = input(" Filepath >> ")
+if _type == "c":
+    _user = f"C:\\Users\\{os.getlogin()}\\"
+elif _type == "g":
+    _user = "G:\\My Drive\\"
+else:
+    exit()
 sx3.setProperty("voice", "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-US_DAVID_11.0")
 sx3.setProperty("rate", 150)
-_request = input(">> ")
-_wav = input("Filepath >> ")
 sx3.save_to_file(_request, f"{_user}{_wav}.wav")
 sx3.runAndWait()

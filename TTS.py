784de2f6e90231e1de27942e7fdf389350c8638a
ASillyLittleCoder <souid.youssef07@gmail.com>
2025-09-14 17:34:32 +0100

    Add files via upload

--- TTS.py
+++ TTS.py
@@ -1,17 +1,15 @@
 import pyttsx3
 import os
-from sys import exit
+import inquirer
 sx3 = pyttsx3.init()
-_type = input(" Drive or Computer ( g / c ) >> ")
-_voice = input(" Microsoft Voice >>")
+_type = inquirer.list_input("Drive or Computer (G: / C:)", choices = ["Google Drive", "Computer"])
+_voice = inquirer.list_input("Microsoft Voice", choices = ["Hortense", "David", "Zira", "Hazel", "Heami"])
 _request = input(" Insert Audio >> ")
 _wav = input(" Filepath >> ")
-if _type == "c":
+if _type == "Computer":
     _user = f"C:\\Users\\{os.getlogin()}\\"
-elif _type == "g":
+elif _type == "Google Drive":
     _user = "G:\\My Drive\\"
-else:
-    exit()
 if _voice == "Hortense":
     sx3.setProperty("voice", "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_FR-FR_HORTENSE_11.0")
 elif _voice == "David":
@@ -21,9 +19,7 @@ elif _voice == "Zira":
 elif _voice == "Hazel":
     sx3.setProperty("voice", "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_EN-GB_HAZEL_11.0")
 elif _voice == "Heami":
-   sx3.setProperty("HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_KO-KR_HEAMI_11.0")
-else:
-    exit() 
+   sx3.setProperty("voice", "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\TTS_MS_KO-KR_HEAMI_11.0")
 sx3.setProperty("rate", 150)
 sx3.save_to_file(_request, f"{_user}{_wav}.wav")
 sx3.runAndWait()
